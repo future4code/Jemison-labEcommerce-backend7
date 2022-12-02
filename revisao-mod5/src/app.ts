58296d3aeb0fcc89636b266ccebc8254a47ec874
Leonardo Koga <leokoga@gmail.com>
2022-12-01 22:16:25 -0300

    exercicio completo

--- revisao-mod5/src/app.ts
+++ revisao-mod5/src/app.ts
@@ -1,12 +1,15 @@
+import dotenv from 'dotenv';
 import express, {Express} from "express"
 import cors from "cors"
+
+dotenv.config();
 
 const app: Express = express()
 
 app.use(express.json())
 app.use(cors())
 
-app.listen(3003, ()=>{
+app.listen(process.env.PORT || 3003, ()=>{
     console.log("Server ready!")
 })
 
--- revisao-mod5/src/endpoints/getAllCharacters.ts
+++ revisao-mod5/src/endpoints/getAllCharacters.ts
@@ -9,12 +9,25 @@ export default async function getAllCharacters(
 ):Promise<void>{
 
     try {
-        const name = req.query
+        const { name, orderBy, orderType, page } = req.query
 
-        //const result: character[] = await connection.raw("SELECT * FROM character")
-        //res.send(result[0])
+        // const result: character[] = await connection.raw("SELECT * FROM character")
+        // res.send(result[0])
+
+        const resultsPerPage = 5
+        
+        // página 1 -> offset 0 === 5 * 0
+        // página 2 -> offset 5 === 5 * 1
+        // página 3 -> offset 10 === 5 * 2
+
+        const offset = resultsPerPage * (Number(page)-1)
+        
+        debugger
 
         const characters: character[] = await connection("character")
+            .where("name", "LIKE", `%${name}%`)
+            .orderBy(orderBy as string || "name", orderType as string)
+            .offset(offset)
 
         resp.send(characters  )
     } catch (error) {
