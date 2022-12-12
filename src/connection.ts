ae950116138f33b0504ca942c2891267a3d53fa7
Leonardo Koga <leokoga@gmail.com>
2022-12-12 18:47:17 -0300

    correcao

--- src/connection.ts
+++ src/connection.ts
@@ -10,7 +10,7 @@ const connection = knex({
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_SCHEMA,
-        port: Number(process.env.DB_PORT),
+        port: 3306,
         multipleStatements: true
     }
 })
