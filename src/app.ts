d40192715d366f15e7de3d06a30a51e343dc93c5
Leonardo Koga <leokoga@gmail.com>
2023-01-05 21:45:46 -0300

    teste depois da entrega

--- src/app.ts
+++ src/app.ts
@@ -9,12 +9,12 @@ const app: Express = express()
 app.use(express.json())
 app.use(cors())
 
-// app.listen(process.env.PORT || 3003, ()=>{
-//     console.log("Server ready!")
-// })
+app.listen(process.env.PORT || 3003, ()=>{
+    console.log("Server ready!")
+})
 
-app.listen(3003, () => {
-    console.log(`Servidor rodando na porta ${3003}`)
-  })
+// app.listen(3003, () => {
+//     console.log(`Servidor rodando na porta ${3003}`)
+//   })
 
 export default app
